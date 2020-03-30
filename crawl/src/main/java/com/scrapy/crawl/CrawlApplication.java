0a1e64601bc6bc07a78d378f245e495f195765cf
zhangjie <zhangjie@xnskg.cn>
2020-03-30 10:41:40 +0800

    更新前备份

--- crawl/src/main/java/com/scrapy/crawl/CrawlApplication.java
+++ crawl/src/main/java/com/scrapy/crawl/CrawlApplication.java
@@ -1,9 +1,11 @@
 package com.scrapy.crawl;
 
+import com.scrapy.crawl.unit.CrosFilter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -17,5 +19,19 @@ public class CrawlApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrawlApplication.class, args);
 	}
+
+
+	/**
+	 * 配置跨域访问的过滤器
+	 * @return
+	 */
+	@Bean
+	public FilterRegistrationBean registerFilter(){
+		FilterRegistrationBean bean = new FilterRegistrationBean();
+		bean.addUrlPatterns("/*");
+		bean.setFilter(new CrosFilter());
+		return bean;
+	}
+
 
 }
